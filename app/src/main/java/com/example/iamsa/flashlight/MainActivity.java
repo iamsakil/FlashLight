28d555ba8f89caa56137bc9e0c195e13c7555eee
Sakil Mallick <sakilmallick094@gmail.com>
2017-11-06 00:54:15 +0530

    Initial commit

---
+++ app/src/main/java/com/example/iamsa/flashlight/MainActivity.java
@@ -0,0 +1,146 @@
+package com.example.iamsa.flashlight;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private CameraManager mCameraManager;
+    private String mCameraId;
+    private Boolean isTorchOn;
+    @Override
+    @TargetApi(21)
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d("FlashLightActivity", "onCreate()");
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_main);
+        isTorchOn = false;
+
+        Boolean isFlashAvailable = getApplicationContext().getPackageManager()
+                .hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+
+        if (!isFlashAvailable) {
+
+            AlertDialog alert = new AlertDialog.Builder(MainActivity.this)
+                    .create();
+            alert.setTitle("Error !!");
+            alert.setMessage("Your device doesn't support offbtn light!");
+            alert.setButton(DialogInterface.BUTTON_POSITIVE, "OK", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    // closing the application
+                    finish();
+                    System.exit(0);
+                }
+            });
+            alert.show();
+            return;
+        }
+
+        mCameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        try {
+            mCameraId = mCameraManager.getCameraIdList()[0];
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+        final Button onBtn = (Button) findViewById(R.id.onButton);
+        onBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    if (isTorchOn) {
+                        turnOffFlashLight();
+                        isTorchOn = false;
+                    } else {
+                        turnOnFlashLight();
+                        isTorchOn = true;
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+            Camera cam = null;
+            private void turnOffFlashLight() {
+                try {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        mCameraManager.setTorchMode(mCameraId, false);
+                    }
+                    else {
+                        try {
+                            Handler handler = new Handler();
+                            handler.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Camera camera = Camera.open();
+                                    android.hardware.Camera.Parameters parameters = camera.getParameters();
+
+                                    parameters.setFlashMode(android.hardware.Camera.Parameters.FLASH_MODE_OFF);
+                                    camera.setParameters(parameters);
+
+                                    camera.stopPreview();
+                                    camera.release();
+                                }
+                            });
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                            Toast.makeText(getBaseContext(), "Exception throws in turning off flashlight.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    onBtn.setBackgroundResource(R.drawable.offbtn);
+                } catch (CameraAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            private void turnOnFlashLight() {
+                try {
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        mCameraManager.setTorchMode(mCameraId, true);
+                    }
+                    else {
+                        try {
+                            Camera camera = Camera.open();
+                            android.hardware.Camera.Parameters parameters = camera.getParameters();
+
+                            parameters.setFlashMode(android.hardware.Camera.Parameters.FLASH_MODE_TORCH);
+                            camera.setParameters(parameters);
+                            camera.startPreview();
+
+                            camera.autoFocus(new android.hardware.Camera.AutoFocusCallback() {
+                                @Override
+                                public void onAutoFocus(boolean b, android.hardware.Camera camera) {
+
+                                }
+                            });
+                        } catch (RuntimeException e) {
+                            Toast.makeText(MainActivity.this,"Camera Permission is not granted",Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    onBtn.setBackgroundResource(R.drawable.onbtn);
+                } catch (CameraAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+}
